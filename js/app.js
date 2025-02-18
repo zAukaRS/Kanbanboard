4af1aa747d292c7f3e893e7f3567a4fbc5400afd
zAukaRS <evgenii-savostin10@mail.ru>
2025-02-19 02:33:53 +0700

    function task(add, edit, delete, move)

--- js/app.js
+++ js/app.js
@@ -11,4 +11,32 @@ new Vue({
             newTask: {title: '', description: '', deadline: ''}
         };
     },
+    methods: {
+        addTask() {
+            if (this.newTask.title && this.newTask.description && this.newTask.deadline) {
+                this.columns[0].tasks.push({
+                    ...this.newTask,
+                    createdAt: new Date(),
+                    lastEdited: new Date()
+                });
+                this.newTask = { title: '', description: '', deadline: '' };
+            }
+        },
+        editTask(task) {
+            task.lastEdited = new Date();
+        },
+        deleteTask(task, columnIndex) {
+            const index = this.columns[columnIndex].tasks.indexOf(task);
+            if (index > -1) {
+                this.columns[columnIndex].tasks.splice(index, 1);
+            }
+        },
+        moveTask(task, fromColumn, toColumn) {
+            const index = this.columns[fromColumn].tasks.indexOf(task);
+            if (index > -1) {
+                this.columns[fromColumn].tasks.splice(index, 1);
+                this.columns[toColumn].tasks.push({ ...task, lastEdited: new Date() });
+            }
+        }
+    }
 });
